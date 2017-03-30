6fa6fcd6d7b656ca288ffb80ded2df510627194a
Cyril Waechter <cyrilwaechter@users.noreply.github.com>
2017-03-31 00:53:26 +0200

    Add simple sample form using which use an ExternalEvent

--- pyRevitMEP.tab/Samples.panel/FormExternalEventHandler.pushbutton/script.py
+++ pyRevitMEP.tab/Samples.panel/FormExternalEventHandler.pushbutton/script.py
@@ -22,41 +22,52 @@ from Autodesk.Revit.UI import IExternalEventHandler, ExternalEvent
 from Autodesk.Revit.DB import Transaction
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit.Exceptions import InvalidOperationException
-from revitutils import selection, doc
+from revitutils import selection, uidoc, doc
 from scriptutils.userinput import WPFWindow
 
 __doc__ = "A simple modeless form sample"
 __title__ = "Modeless Form"
 __author__ = "Cyril Waechter"
 
-uidoc = __revit__.ActiveUIDocument
-
+# Simple function we want to run
 def delete_elements():
-    t = Transaction(doc, "ModelessDeletion")
+    t = Transaction(doc, "Delete selection")
     t.Start()
     for elid in uidoc.Selection.GetElementIds():
         print elid
         doc.Delete(elid)
     t.Commit()
 
-
+# Create a subclass of IExternalEventHandler
 class SimpleEventHandler(IExternalEventHandler):
+    """
+    Simple IExternalEventHandler sample
+    """
+
+    # __init__ is used to make function from outside of the class to be executed by the handler. \
+    # Instructions could be simply written under Execute method only
     def __init__(self, do_this):
         self.do_this = do_this
 
+    # Execute method run in Revit API environment.
     def Execute(self, uiapp):
         try:
             self.do_this()
         except InvalidOperationException:
+            # If you don't catch this exeption Revit may crash.
             print "InvalidOperationException catched"
 
     def GetName(self):
-        return "simple form event"
+        return "simple function executed by an IExternalEventHandler in a Form"
 
+
+# Now we need to make an instance of this handler. Moreover, it shows that the same class could be used to for
+# different functions using different handler class instances
 simple_event_handler = SimpleEventHandler(delete_elements)
-ex_event = ExternalEvent.Create(simple_event_handler)
+# We now need to create the ExternalEvent
+ext_event = ExternalEvent.Create(simple_event_handler)
 
-
+# A simple WPF form used to call the ExternalEvent
 class ModelessForm(WPFWindow):
     """
     Simple modeless form sample
@@ -68,8 +79,8 @@ class ModelessForm(WPFWindow):
         self.Show()
 
     def delete_click(self, sender, e):
-        for el in selection.elements:
-            print el
-        ex_event.Raise()
+        # This Raise() method launch a signal to Revit to tell him you want to do something in the API context
+        ext_event.Raise()
 
+# Let's launch our beautiful and useful form !
 modeless_form = ModelessForm("ModelessForm.xaml")
