191d4182212db3f5240f717da32b98ad5ac747e4
Cyril Waechter <cyrilwaechter@users.noreply.github.com>
2017-07-15 10:37:40 +0200

    ViewRename rework started

--- pyRevitMEP.tab/Tools.panel/ViewRename.pushbutton/script.py
+++ pyRevitMEP.tab/Tools.panel/ViewRename.pushbutton/script.py
@@ -17,9 +17,10 @@ See this link for a copy of the GNU General Public License protecting this packa
 https://github.com/CyrilWaechter/pyRevitMEP/blob/master/LICENSE
 """
 from revitutils import doc, selection
+from scriptutils.userinput import WPFWindow
 
 # noinspection PyUnresolvedReferences
-from Autodesk.Revit.DB import Transaction, Element, BuiltInParameter
+from Autodesk.Revit.DB import Transaction, Element, BuiltInParameter, FilteredElementCollector, ViewPlan
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit import Exceptions
 
@@ -63,3 +64,50 @@ except:  # print a stack trace and error messages for debugging
     import traceback
     traceback.print_exc()
     t.RollBack()
+
+def get_viewparameters(viewclass):
+    view = FilteredElementCollector(doc).OfClass(viewclass).FirstElement().Parameters
+    for param in view.Parameters:
+        print(param.Definition.Name)
+        print(param.Definition.BuiltInParameter)
+        if param.IsShared:
+            print(param.GUID)
+        print param.AsValueString()
+
+class ViewRename(WPFWindow):
+    """
+    GUI used to select a reference level from a list or an object
+    """
+
+    def __init__(self, xaml_file_name):
+        WPFWindow.__init__(self, xaml_file_name)
+
+        # self.combobox_parameters.ItemsSource = get_viewparameters(ViewPlan)
+        self.viewname_preview = self.pattern.Text
+        self.preview.DataContext = self.viewname_preview
+        self.cursorposition = 0
+        self.cb_all.IsChecked = None
+
+    def button_addparameter_click(self, sender, e):
+        parameter = "test"
+        self.pattern.Text = self.pattern.Text[:self.cursorposition]+parameter+self.pattern.Text[self.cursorposition:]
+
+    def pattern_SelectionChanged(self, sender, e):
+        self.cursorposition = sender.SelectionStart
+        print sender.Text
+        self.preview.DataContext = self.pattern.Text
+
+    def cb_CheckedChanged(self, sender, e):
+        self.cb_all.IsChecked = None
+        if self.cb_viewplan.IsChecked == True and self.cb_view3D.IsChecked == True and self.cb_viewsection.IsChecked == True :
+            self.cb_all.IsChecked = True
+        if self.cb_viewplan.IsChecked == False and self.cb_view3D.IsChecked == False and self.cb_viewsection.IsChecked == False :
+            self.cb_all.IsChecked = False
+
+    def cb_all_CheckedChanged(self, sender, e):
+        checked = self.cb_all.IsChecked
+        self.cb_viewplan.IsChecked = checked
+        self.cb_view3D.IsChecked = checked
+        self.cb_viewsection.IsChecked = checked
+
+ViewRename('ViewRename.xaml').ShowDialog()
