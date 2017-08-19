4ce9ac8d3316e04549d91593c9e15977759758a6
Cyril Waechter <cyrilwaechter@users.noreply.github.com>
2017-08-19 22:19:11 +0200

    Added unperfect evaporate and freeze temperature computation. Corrected incompressible fluid formatting for CoolProp inputs.

--- pyRevitMEP.tab/Fluid.panel/FluidCreate.pushbutton/script.py
+++ pyRevitMEP.tab/Fluid.panel/FluidCreate.pushbutton/script.py
@@ -8,6 +8,7 @@ from Autodesk.Revit.DB import Transaction, UnitUtils, DisplayUnitType
 from Autodesk.Revit.DB.Plumbing import FluidType, FluidTemperature
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit.UI import TaskDialog, TaskDialogCommonButtons, TaskDialogResult
+from scriptutils import logger
 from scriptutils.userinput import WPFWindow
 import ctypes
 import os
@@ -22,14 +23,16 @@ Label = rpw.ui.forms.flexform.Label
 TextBox = rpw.ui.forms.flexform.TextBox
 Button = rpw.ui.forms.flexform.Button
 
-#Load CoolProp shared library and configure PropsSI c_types units
+# Load CoolProp shared library and configure PropsSI c_types units
 CP = ctypes.cdll.LoadLibrary(os.path.join(__commandpath__, "CoolProp_x64.dll"))
 PropsSI = CP.PropsSI
-PropsSI.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_double, ctypes.c_char_p, ctypes.c_double, ctypes.c_char_p)
+PropsSI.argtypes = (ctypes.c_char_p,  # searched value. Example 'V' (Viscosity)
+                    ctypes.c_char_p, ctypes.c_double,  # Fixed propriety 1. Example for temperature in K : 'T', 273.15
+                    ctypes.c_char_p, ctypes.c_double,  # Fixed propriety 2. Example for pressure in Pa : 'P', 101325
+                    ctypes.c_char_p)  # Fluid name
 PropsSI.restype = ctypes.c_double
 
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
+doc = rpw.revit.doc
 
 fluid_list = {'Pure':
                   ('1-Butene', 'Acetone', 'Air', 'Ammonia', 'Argon', 'Benzene', 'CarbonDioxide', 'CarbonMonoxide',
@@ -55,11 +58,17 @@ fluid_list = {'Pure':
                    'RC318', 'SES36', 'SulfurDioxide', 'SulfurHexafluoride', 'Toluene', 'trans-2-Butene', 'Water',
                    'Xenon'),
               'Incompressible : Pure':
-                  ("AS10","AS20","AS30","AS40","AS55","DEB","DowJ","DowJ2","DowQ","DowQ2","HC10","HC20","HC30","HC40","HC50","HCB","HCM","HFE","HFE2","HY20","HY30","HY40","HY45","HY50","NBS","NaK","PBB","PCL","PCR","PGLT","PHE","PHR","PLR","PMR","PMS1","PMS2","PNF","PNF2","S800","SAB","T66","T72","TCO","TD12","TVP1","TVP1869","TX22","TY10","TY15","TY20","TY24","Water","XLT","XLT2","ZS10","ZS25","ZS40","ZS45","ZS55"),
+                  ("AS10", "AS20", "AS30", "AS40", "AS55", "DEB", "DowJ", "DowJ2", "DowQ", "DowQ2", "HC10", "HC20",
+                   "HC30", "HC40", "HC50", "HCB", "HCM", "HFE", "HFE2", "HY20", "HY30", "HY40", "HY45", "HY50", "NBS",
+                   "NaK", "PBB", "PCL", "PCR", "PGLT", "PHE", "PHR", "PLR", "PMR", "PMS1", "PMS2", "PNF", "PNF2",
+                   "S800", "SAB", "T66", "T72", "TCO", "TD12", "TVP1", "TVP1869", "TX22", "TY10", "TY15", "TY20",
+                   "TY24", "Water", "XLT", "XLT2", "ZS10", "ZS25", "ZS40", "ZS45", "ZS55"),
               'Incompressible : Mixtures (vol)':
-                  ("AEG","AKF","AL","AN","APG","GKN","PK2","PKL","ZAC","ZFC","ZLC","ZM","ZMC"),
+                  ("AEG", "AKF", "AL", "AN", "APG", "GKN", "PK2", "PKL", "ZAC", "ZFC", "ZLC", "ZM", "ZMC"),
               'Incompressible : Mixtures (%)':
-                  ("FRE","IceEA","IceNA","IcePG","LiBr","MAM","MAM2","MCA","MCA2","MEA","MEA2","MEG","MEG2","MGL","MGL2","MITSW","MKA","MKA2","MKC","MKC2","MKF","MLI","MMA","MMA2","MMG","MMG2","MNA","MNA2","MPG","MPG2","VCA","VKC","VMA","VMG","VNA")
+                  ("FRE", "IceEA", "IceNA", "IcePG", "LiBr", "MAM", "MAM2", "MCA", "MCA2", "MEA", "MEA2", "MEG", "MEG2",
+                   "MGL", "MGL2", "MITSW", "MKA", "MKA2", "MKC", "MKC2", "MKF", "MLI", "MMA", "MMA2", "MMG", "MMG2",
+                   "MNA", "MNA2", "MPG", "MPG2", "VCA", "VKC", "VMA", "VMG", "VNA")
               }
 
 
@@ -88,8 +97,50 @@ def freeze_temp(fluid_name, pressure=101325):
     :param pressure: defined pressure in Pa, default=101325 (1 atm)
     :return: freezing temperature in K
     """
-    return PropsSI('T_FREEZE','T',275.15,'P',pressure, fluid_name)
+    logger.debug(fluid_name)
+    if "INCOMP::" not in fluid_name:
+        return 'unknown'
+    else:
+        temp = 273.15 + 50
+        if PropsSI("V", "T", temp, "P", pressure, fluid_name) == float('Inf'):
+            return 'unknown'
+        while True:
+            # Check if CoolProp return a valid value. Else freeze_temp is reached.
+            value = PropsSI("V", "T", temp, "P", pressure, fluid_name)
+            logger.debug("{} : {}".format(type(value), value))
+            if value != float('Inf'):
+                temp -= 1
+                continue
+            else:
+                temp += 1
+                break
+    return temp
 
+
+def evaporation_temp(fluid_name, pressure=101325):
+    """
+    Return freezing temperature of a given fluid at a given temperature
+    :param fluid_name: fluid name including concentration if mixture
+    :param pressure: defined pressure in Pa, default=101325 (1 atm)
+    :return: freezing temperature in K
+    """
+    logger.debug(fluid_name)
+    if "INCOMP::" not in fluid_name:
+        temp = PropsSI('T', 'P', pressure, 'Q', 0, fluid_name)
+    else:
+        temp = 273.15 + 50
+        if PropsSI("V", "T", temp, "P", pressure, fluid_name) == float('Inf'):
+            return 'unknown'
+        while True:
+            value = PropsSI("V", "T", temp, "P", pressure, fluid_name)
+            logger.debug("{} : {}".format(type(value), value))
+            if value != float('Inf'):
+                temp += 1
+                continue
+            else:
+                temp -= 1
+                break
+    return temp
 
 
 def add_temperatures(t_start, t_end, fluid_type, coolprop_fluid, pressure, t_init=273.15):
@@ -99,22 +150,26 @@ def add_temperatures(t_start, t_end, fluid_type, coolprop_fluid, pressure, t_ini
     """
     t = Transaction(doc, "Add temperatures")
     t.Start()
-    for i in xrange(t_start, t_end+1):
-        #Call CoolProp to get fluid properties and convert it to internal units if necessary
-        temperature = t_init+i
+    for i in xrange(t_start, t_end + 1):
+        # Call CoolProp to get fluid properties and convert it to internal units if necessary
+        temperature = t_init + i
         viscosity = UnitUtils.ConvertToInternalUnits(PropsSI('V', 'T', temperature, 'P', pressure, coolprop_fluid),
                                                      DisplayUnitType.DUT_PASCAL_SECONDS)
-        density = UnitUtils.ConvertToInternalUnits(PropsSI('D','T', temperature,'P', pressure, coolprop_fluid),
+        density = UnitUtils.ConvertToInternalUnits(PropsSI('D', 'T', temperature, 'P', pressure, coolprop_fluid),
                                                    DisplayUnitType.DUT_KILOGRAMS_PER_CUBIC_METER)
-        #Catching exceptions and trying to overwrite temperature if asked by user in the TaskDialog
+        logger.debug('ν={}, ρ={}'.format(viscosity, density))
+        # Catching exceptions and trying to overwrite temperature if asked by user in the TaskDialog
         try:
-            fluid_type.AddTemperature(FluidTemperature(temperature,viscosity,density))
+            fluid_type.AddTemperature(FluidTemperature(temperature, viscosity, density))
         except Exceptions.ArgumentException:
-            result = TaskDialog.Show("Error", "Temperature already exist, do you want to overwrite it ?", TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No | TaskDialogCommonButtons.Cancel, TaskDialogResult.Yes)
+            result = TaskDialog.Show("Error", "Temperature already exist, do you want to overwrite it ?",
+                                     TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No |
+                                     TaskDialogCommonButtons.Cancel,
+                                     TaskDialogResult.Yes)
             if result == TaskDialogResult.Yes:
                 try:
                     fluid_type.RemoveTemperature(temperature)
-                    fluid_type.AddTemperature(FluidTemperature(temperature,viscosity,density))
+                    fluid_type.AddTemperature(FluidTemperature(temperature, viscosity, density))
                 except Exceptions.ArgumentException:
                     TaskDialog.Show("Overwrite error", "Temperature is currently in use and cannot be overwritten")
             elif result == TaskDialogResult.No:
@@ -122,6 +177,7 @@ def add_temperatures(t_start, t_end, fluid_type, coolprop_fluid, pressure, t_ini
             else:
                 break
     t.Commit()
+
 
 class FluidSelection(WPFWindow):
     """
@@ -139,17 +195,16 @@ class FluidSelection(WPFWindow):
     def fluid_category_changed(self, sender, e):
         self.cb_fluid_name.ItemsSource = fluid_list[self.cb_fluid_category.SelectedItem]
 
-    def fluid_name_changed(self, sender, e):
+    # noinspection PyUnusedLocal
+    def fluid_property_changed(self, sender, e):
         pressure = float(self.txt_pressure.Text)
         fluid_concentration = float(self.txt_concentration.Text)
-        coolprop_fluid = sender.SelectedItem
+        coolprop_fluid = self.cb_fluid_name.SelectedItem
         if coolprop_fluid is not None:
-            if 'incompressible' in coolprop_fluid.lower():
+            if 'incompressible' in self.cb_fluid_category.SelectedItem.lower():
                 coolprop_fluid = 'INCOMP::{0}[{1}]'.format(coolprop_fluid, fluid_concentration)
-            try:
-                self.freeze_temp_text.Text = str(freeze_temp(coolprop_fluid, pressure))
-            except:
-                self.freeze_temp_text.Text = str(0)
+            self.freeze_temp_text.Text = str(freeze_temp(coolprop_fluid, pressure))
+            self.evaporate_temp_text.Text = str(evaporation_temp(coolprop_fluid, pressure))
 
     # noinspection PyUnusedLocal
     def add_temperature_click(self, sender, e):
@@ -166,12 +221,11 @@ class FluidSelection(WPFWindow):
         fluid_type = create_fluid_type(revit_fluid_name)
 
         # If it is an incompressible fluid. CoolProp need a special format.
-        if 'incompressible' in coolprop_fluid.lower():
+        if 'incompressible' in self.cb_fluid_category.SelectedItem.lower():
             coolprop_fluid = 'INCOMP::{0}[{1}]'.format(coolprop_fluid, fluid_concentration)
 
         # Finally add temperatures to Revit Project
         add_temperatures(t_start, t_end, fluid_type, coolprop_fluid, pressure)
 
+
 FluidSelection('FluidSelection.xaml').ShowDialog()
-
-
