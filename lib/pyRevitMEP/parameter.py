11f8e022039d2298412fe1fa38630100d05e7ad1
Cyril Waechter <cyrilwaechter@users.noreply.github.com>
2017-11-16 17:34:27 +0100

    New SharedParameter to handle reading and creating shared parameters

--- lib/pyRevitMEP/parameter.py
+++ lib/pyRevitMEP/parameter.py
@@ -1,6 +1,91 @@
 # coding: utf8
 import rpw
+# noinspection PyUnresolvedReferences
+from Autodesk.Revit.DB import ParameterType
+# noinspection PyUnresolvedReferences
 from rpw import revit, DB, UI
+# noinspection PyUnresolvedReferences
+from System import Guid
+import csv
+
+
+class SharedParameter:
+    def __init__(self, name, group, parameter_type, visible=True, guid=None):
+        if not guid:
+            self.guid = Guid.NewGuid()
+        else:
+            self.guid = guid
+        self.visible = visible
+        if isinstance(ParameterType.Text, ParameterType):
+            self.type = type
+        elif parameter_type in ParameterType.GetNames(DB.ParameterType):
+            self.type = getattr(ParameterType, parameter_type)
+        else:
+            selected_type = rpw.ui.forms.SelectFromList("Select ParameterType",
+                                                        ParameterType.GetNames(ParameterType),
+                                                        "Invalid ParameterType please select a parameter type")
+            self.type = getattr(ParameterType, selected_type)
+        self.group = group
+        self.name = name
+
+    @staticmethod
+    def read_from_csv(csv_path=None):
+        if not csv_path:
+            csv_path = rpw.ui.forms.select_file(extensions='csv Files (*.csv*)|*.csv*', title='Select File',
+                                                multiple=False, restore_directory=True)
+
+        csv_file = open(csv_path)
+        file_reader = csv.reader(csv_file)
+
+        shared_parameter_list = []
+
+        for row in file_reader:
+            row_len = len(row)
+            if row_len < 3:
+                print("Line {} is invalid, less than 3 column".format(file_reader.line_num))
+            name, group, type = row[0:3]
+            visible = True
+            guid = None
+            if row_len > 4:
+                visible = bool(row[3])
+            if row_len > 5:
+                guid = Guid(row[4])
+
+            shared_parameter_list.append(SharedParameter(name, group, type, visible, guid))
+            print(shared_parameter_list)
+
+        return shared_parameter_list
+
+    def write_to_definition_file(self, warning=True):
+        """
+        Create a new parameter definition in cVrrent shared parameter file
+        :param warning: warn user if a definition with given name already exist
+        :return: definition
+        """
+        definition_file = revit.app.OpenSharedParameterFile()
+        if not definition_file:
+            raise LookupError("No shared parameter file")
+
+        for dg in definition_file.Groups:
+            if dg.Name == self.group:
+                definition_group = dg
+                break
+        else:
+            definition_group = definition_file.Groups.Create(self.group)
+
+        for definition in definition_group.Definitions:
+            if definition.Name == self.name:
+                if warning:
+                    print("A parameter definition named {} already exist")
+                break
+        else:
+            external_definition_create_options = DB.ExternalDefinitionCreationOptions(self.name,
+                                                                                      self.type,
+                                                                                      GUID=self.guid,
+                                                                                      Visible=self.visible)
+            definition = definition_group.Definitions.Create(external_definition_create_options)
+
+        return definition
 
 
 def create_shared_parameter_definition(revit_app, name, group_name, parameter_type, visible=True):
--- pyRevitMEP.tab/Lab.panel/BatchCreateSharedParameter.pushbutton/script.py
+++ pyRevitMEP.tab/Lab.panel/BatchCreateSharedParameter.pushbutton/script.py
@@ -1,12 +1,15 @@
 # coding: utf8
 import rpw
 # noinspection PyUnresolvedReferences
-from rpw import DB, UI
+from rpw import revit, DB, UI
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit.Exceptions import InvalidOperationException
 from scriptutils import logger
 from scriptutils.userinput import WPFWindow
-import re
+import csv
+from pyRevitMEP.parameter import SharedParameter
+# noinspection PyUnresolvedReferences
+from System.Collections.Generic import List
 
 __doc__ = "Batch create project shared parameters from file"
 __title__ = "BatchCreateSharedParameters"
@@ -14,23 +17,29 @@ __author__ = "Cyril Waechter"
 
 doc = rpw.revit.doc
 uidoc = rpw.uidoc
-TextBox = rpw.ui.forms.TextBox
 
-file = rpw.ui.forms.select_file(extensions='All Files (*.*)|*.*', title='Select File',
-                                multiple=False, restore_directory=True)
-with open(file, "r") as text:
-    text_text = text.read()
+# text_box = TextBox("parameters_text",text_text, Height=400, Width=400)
+# components = [text_box]
+#
+# form = rpw.ui.forms.FlexForm("Parameters to create check", components)
+# form.ShowDialog()
+#
+# parameters_text = text_box.Text.split("\n")
+#
+# for parameter in parameters_text:
+#     print parameter
+#
+# with rpw.db.Transaction("Batch create shared parameters from file"):
+#      for parameter_name in parameters_text:
+#         SharedParameter(revit.app, parameter_name, "MCR", DB.ParameterType.Text)
 
-text_box = TextBox("parameters_text",text_text, Height=400, Width=400)
-components = [text_box]
 
-form = rpw.ui.forms.FlexForm("Parameters to create check", components)
-form.ShowDialog()
+class Gui(WPFWindow):
+    def __init__(self, xaml_file_name):
+        WPFWindow.__init__(self, xaml_file_name)
+        self.datagrid.ItemsSource = SharedParameter.read_from_csv()
 
-parameters_text = text_box.Text.split("\n")
 
-for parameter in parameters_text:
-    print parameter
 
-# with rpw.db.Transaction("Batch create worksets from file"):
-#     pass
+gui = Gui("WPFWindow.xaml")
+gui.ShowDialog()
