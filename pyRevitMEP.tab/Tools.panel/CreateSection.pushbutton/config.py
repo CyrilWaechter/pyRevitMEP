345b6d6e33d5308fb55489d23d00d6f9c97796f9
Cyril Waechter <cyrilwaechter@users.noreply.github.com>
2018-07-26 15:05:28 +0200

    Create section : Release. Added options accessed by Shift-click Reworked section type selection

---
+++ pyRevitMEP.tab/Tools.panel/CreateSection.pushbutton/config.py
@@ -0,0 +1,61 @@
+# coding: utf8
+
+import System
+
+from Autodesk.Revit.DB import Document, UnitUtils, UnitType, UnitSymbolType, LabelUtils, FormatOptions, DisplayUnitType
+
+import rpw
+from pyrevit import forms, script
+
+doc = rpw.revit.doc  # type: Document
+
+unit_format_options = doc.GetUnits().GetFormatOptions(UnitType.UT_Length)
+
+
+display_unit = unit_format_options.DisplayUnits
+symbol_type = unit_format_options.UnitSymbol
+if symbol_type == UnitSymbolType.UST_NONE:
+    try:
+        symbol_type = FormatOptions.GetValidUnitSymbols(display_unit).Item[1]
+    except System.ArgumentOutOfRangeException:
+        display_unit = DisplayUnitType.DUT_DECIMAL_FEET
+        symbol_type = FormatOptions.GetValidUnitSymbols(display_unit).Item[1]
+symbol = LabelUtils.GetLabelFor(symbol_type)
+
+def import_config_length(length):
+    return str(UnitUtils.ConvertFromInternalUnits(float(length), display_unit))
+
+def export_config_length(lenght):
+    return str(UnitUtils.ConvertToInternalUnits(float(lenght), display_unit))
+
+class CreateSectionOptions(forms.WPFWindow):
+    def __init__(self):
+        forms.WPFWindow.__init__(self, "CreateSectionOptions.xaml")
+        self.set_image_source("diagram_img", "diagram.png")
+
+        self.tblock_units.Text = "All length in {}".format(symbol)
+
+        # get parameters from config file or use default values
+        self._config = script.get_config()
+
+        self.tbox_prefix.Text = self._config.get_option('prefix', 'Mur')
+        self.tbox_depth_offset.Text = import_config_length(self._config.get_option('depth_offset', '1'))
+        self.tbox_height_offset.Text = import_config_length(self._config.get_option('height_offset', '1'))
+        self.tbox_width_offset.Text = import_config_length(self._config.get_option('width_offset', '1'))
+
+    def save_options(self, sender, e):
+        self._config.prefix = self.tbox_prefix.Text
+        self._config.depth_offset = export_config_length(self.tbox_depth_offset.Text)
+        self._config.height_offset = export_config_length(self.tbox_height_offset.Text)
+        self._config.width_offset = export_config_length(self.tbox_width_offset.Text)
+        script.save_config()
+        self.Close()
+
+    def reset_options(self, sender, e):
+        script.reset_config()
+        self.Close()
+        reseted_gui = CreateSectionOptions().show_dialog()
+
+
+options_gui = CreateSectionOptions().show_dialog()
+
--- pyRevitMEP.tab/Tools.panel/CreateSection.pushbutton/script.py
+++ pyRevitMEP.tab/Tools.panel/CreateSection.pushbutton/script.py
@@ -1,10 +1,12 @@
 # coding: utf8
 
-__doc__ = "Quick dimension selected elements"
-__title__ = "QuickDimension"
+__doc__ = """Create sections from selected linear objects (eg. walls)
+SHIFT-CLICK to display options"""
+__title__ = "CreateSectionFrom"
 __author__ = "Cyril Waechter"
 
-from Autodesk.Revit.DB import Wall, Document, Line, FilteredElementCollector, ViewFamilyType, ViewFamily, Element, \
+
+from Autodesk.Revit.DB import Document, Line, FilteredElementCollector, ViewFamilyType, ViewFamily, Element, \
     ViewSection, Transform, BoundingBoxXYZ, XYZ, BuiltInParameter
 from Autodesk.Revit.UI import UIDocument
 from Autodesk.Revit import Exceptions
@@ -17,13 +19,36 @@ doc = rpw.revit.doc  # type: Document
 logger = script.get_logger()
 
 
-section_type = forms.SelectFromList.show(
-    [vt for vt in FilteredElementCollector(doc).OfClass(ViewFamilyType) if vt.ViewFamily == ViewFamily.Section],
-    "Select view type to create section"
-)[0]
-width_offset = 1
-height_offset = 1
-lenght_offset = 1
+class SectionTypeSelection(forms.WPFWindow):
+    def __init__(self):
+        forms.WPFWindow.__init__(self, "SectionTypeSelection.xaml")
+
+        self.combob_selector.DataContext = \
+            [vt for vt in FilteredElementCollector(doc).OfClass(ViewFamilyType) if vt.ViewFamily == ViewFamily.Section]
+
+        self.selected_type = None
+
+    def select_click(self, sender, e):
+        self.selected_type = self.combob_selector.SelectedItem
+        self.Close()
+
+    def show_dialog(self):
+        self.ShowDialog()
+        if self.selected_type:
+            return self.selected_type
+        else:
+            import sys
+            sys.exit()
+
+section_type = SectionTypeSelection().show_dialog()
+
+
+# Retrieve parameters from config file
+_config = script.get_config()
+prefix = _config.get_option('prefix', 'Mur')
+depth_offset = float(_config.get_option('depth_offset', '1'))
+height_offset = float(_config.get_option('height_offset', '1'))
+width_offset = float(_config.get_option('width_offset', '1'))
 
 sections = []
 
@@ -59,11 +84,11 @@ for element_id in uidoc.Selection.GetElementIds():
 
     # Try to retrieve element height, width and lenght
     try:
-        el_width =  element.WallType.Width
+        el_depth =  element.WallType.Width
     except AttributeError:
-        el_width = 2
+        el_depth = 2
 
-    el_length = curve.Length
+    el_width = curve.Length
 
     el_bounding_box = element.get_BoundingBox(None)
     z_min = el_bounding_box.Min.Z
@@ -71,12 +96,12 @@ for element_id in uidoc.Selection.GetElementIds():
     el_height = z_max - z_min
 
     # Set BoundingBoxXYZ's boundaries
-    section_box.Min = XYZ(-el_length / 2 - lenght_offset,
+    section_box.Min = XYZ(-el_width / 2 - width_offset,
                           -height_offset,
-                          -el_width / 2 - width_offset)
-    section_box.Max = XYZ(el_length / 2 + lenght_offset,
+                          -el_depth / 2 - depth_offset)
+    section_box.Max = XYZ(el_width / 2 + width_offset,
                           el_height + height_offset,
-                          el_width / 2 + width_offset)
+                          el_depth / 2 + depth_offset)
 
     # Append necessary parameters to create sections in a list
     sections.append({"box": section_box,
@@ -87,6 +112,6 @@ with rpw.db.Transaction("Create sections", doc):
     for section in sections:
         section_view = ViewSection.CreateSection(doc, section_type.Id, section["box"])
         try:
-            section_view.Name = "Mur {}".format(section["suffix"])
+            section_view.Name = "{} {}".format(prefix, section["suffix"])
         except Exceptions.ArgumentException:
             logger.info("Failed to rename view {}. Desired name already exist.".format(section_view.Name))
