13fb600457011bd98ad9b2ccdff6952718094df8
Cyril Waechter <cyrwae@hotmail.com>
2019-07-18 15:29:21 +0200

    Fix #12

--- pyRevitMEP.tab/Data.panel/ElementChangeLevel.pushbutton/script.py
+++ pyRevitMEP.tab/Data.panel/ElementChangeLevel.pushbutton/script.py
@@ -19,8 +19,9 @@ https://github.com/CyrilWaechter/pypevitmep/blob/master/LICENSE
 
 # noinspection PyUnresolvedReferences
 from Autodesk.Revit.DB import Transaction, BuiltInParameter, Element, Level, MEPCurve, ElementId, FamilyInstance\
-    , FilteredElementCollector
-from pyrevit.forms import WPFWindow
+    , FilteredElementCollector, BuiltInCategory, Category
+from pyrevit import script, HOST_APP
+from pyrevit.forms import WPFWindow, alert
 import rpw
 from rpw import revit
 from pyrevitmep.event import CustomizableEvent
@@ -31,6 +32,28 @@ __author__ = "Cyril Waechter"
 
 doc = revit.doc
 uidoc = revit.uidoc
+
+logger = script.get_logger()
+
+is_revit_2020 = int(HOST_APP.version) >= 2020
+logger.debug(is_revit_2020)
+
+
+def get_enhanced_ids():
+    # Modified category for Revit 2020 and above. When you change level, object doesn't move. eg. fittings.
+    enhanced_cat = (
+        BuiltInCategory.OST_DuctAccessory,
+        BuiltInCategory.OST_DuctFitting,
+        BuiltInCategory.OST_PipeAccessory,
+        BuiltInCategory.OST_PipeFitting,
+        BuiltInCategory.OST_CableTrayFitting,
+        BuiltInCategory.OST_ConduitFitting
+    )
+    return (Category.GetCategory(doc, cat).Id for cat in enhanced_cat)
+
+
+enhanced_cat_ids = get_enhanced_ids()
+logger.debug([cat_id for cat_id in enhanced_cat_ids])
 
 
 def get_level_from_object():
@@ -63,14 +86,17 @@ def change_level(ref_level):
             elif isinstance(el, FamilyInstance) and el.Host is None:
                 el_level = doc.GetElement(el.LevelId)
                 el_level_param = el.get_Parameter(BuiltInParameter.FAMILY_LEVEL_PARAM)
-                el_param_offset = el.get_Parameter(BuiltInParameter.INSTANCE_FREE_HOST_OFFSET_PARAM)
-                el_newoffset = el_param_offset.AsDouble() + el_level.Elevation - ref_level.Elevation
-                el_param_offset.Set(el_newoffset)
+                logger.debug("Category is in enhanced categories ? {}".format(el.Category.Id in enhanced_cat_ids))
+                if not(is_revit_2020 or el.Category.Id in enhanced_cat_ids):
+                    el_param_offset = el.get_Parameter(BuiltInParameter.INSTANCE_FREE_HOST_OFFSET_PARAM)
+                    el_newoffset = el_param_offset.AsDouble() + el_level.Elevation - ref_level.Elevation
+                    el_param_offset.Set(el_newoffset)
                 el_level_param.Set(ref_level.Id)
+
             # Ignore other objects
             else:
-                print "Warning. Following element was ignored. It is probably an hosted element."
-                print el
+                logger.info("Warning. Following element was ignored. It is probably an hosted element.")
+                logger.info(el)
 
 
 customizable_event = CustomizableEvent()
@@ -100,4 +126,10 @@ class ReferenceLevelSelection(WPFWindow):
         customizable_event.raise_event(change_level, level)
 
 
-ReferenceLevelSelection('ReferenceLevelSelection.xaml').Show()
+if __forceddebugmode__:
+    selection = uidoc.Selection.GetElementIds()
+    level = get_level_from_object()
+    uidoc.Selection.SetElementIds(selection)
+    change_level(level)
+else:
+    ReferenceLevelSelection('ReferenceLevelSelection.xaml').Show()
