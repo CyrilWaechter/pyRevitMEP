c3e2f3ed41c26a2f58a263e123956f37abc93d4f
Cyril Waechter <cyrwae@hotmail.com>
2022-10-17 18:14:22 +0200

    Add 2 new tools SpaceVSRoom and MoveSpaceToRoom
    SpaceVSRoom : Check if all room have his corresponding space and vice versa
    MoveSpaceToRoom : Move space to room location by corresponding number

---
+++ pyRevitMEP.tab/Check.panel/SpaceVsRoom.pushbutton/script.py
@@ -0,0 +1,60 @@
+# coding: utf8
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory
+from pyrevit import script, forms, revit
+
+__title__ = "SpaceVsRoom"
+__author__ = "Cyril Waechter"
+__doc__ = "Check if all room have his corresponding space and vice versa"
+
+logger = script.get_logger()
+output = script.get_output()
+
+doc = revit.doc
+room_doc = forms.SelectFromList.show(
+    revit.docs,
+    "Select project which contains rooms",
+    multiselect=False,
+    name_attr="Title",
+)
+
+
+def print_missing(title, missing, base_dict):
+    output.print_md(title)
+    for number in missing:
+        element = base_dict[number]
+        print("{}: {}".format(element.Number, output.linkify(element.Id)))
+
+
+def check_room_vs_space(doc, room_doc):
+    room_dict = {
+        room.Number: room
+        for room in FilteredElementCollector(room_doc).OfCategory(
+            BuiltInCategory.OST_Rooms
+        )
+    }
+    space_dict = {
+        space.Number: space
+        for space in FilteredElementCollector(doc).OfCategory(
+            BuiltInCategory.OST_MEPSpaces
+        )
+    }
+    room_set = set(room_dict.keys())
+    space_set = set(space_dict.keys())
+    missing_room = space_set.difference(room_set)
+    missing_space = room_set.difference(space_set)
+    output.print_md("# Check result")
+    output.print_md(
+        "## {} corresponding room/space found".format(
+            len(room_set.intersection(space_set))
+        )
+    )
+    print_missing(
+        "## Following room have no corresponding space", missing_space, room_dict
+    )
+    print_missing(
+        "## Following space have no corresponding room", missing_room, space_dict
+    )
+
+
+if room_doc:
+    check_room_vs_space(doc, room_doc)
---
+++ pyRevitMEP.tab/Modify.panel/Other.stack/Move.pulldown/MoveSpaceToRoom.pushbutton/script.py
@@ -0,0 +1,52 @@
+# coding: utf8
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory
+from pyrevit import script, forms, revit
+
+__title__ = "SpaceToRoom"
+__author__ = "Cyril Waechter"
+__doc__ = "Move space to room location by corresponding number"
+
+
+class CancelledByUserError(BaseException):
+    pass
+
+
+logger = script.get_logger()
+
+doc = revit.doc
+room_doc = forms.SelectFromList.show(
+    revit.docs,
+    "Select project which contains rooms",
+    multiselect=False,
+    name_attr="Title",
+)
+
+
+def move_space_to_room(doc, room_doc):
+    room_dict = {
+        room.Number: room
+        for room in FilteredElementCollector(room_doc).OfCategory(
+            BuiltInCategory.OST_Rooms
+        )
+    }
+    i = 0
+    with forms.ProgressBar(title="Moving space", cancellable=True) as pb:
+        for space in FilteredElementCollector(doc).OfCategory(
+            BuiltInCategory.OST_MEPSpaces
+        ):
+            if pb.cancelled:
+                raise CancelledByUserError
+            room = room_dict.get(space.Number)
+            if not room:
+                continue
+            i += 1
+            pb.update_progress(i, len(room_dict))
+            space.Location.Move(room.Location.Point - space.Location.Point)
+
+
+if room_doc:
+    with revit.Transaction("Move space to room location", doc) as t:
+        try:
+            move_space_to_room(doc, room_doc)
+        except CancelledByUserError:
+            t._logerror = False
