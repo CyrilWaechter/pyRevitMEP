ccddb0312a3c287bf6882cf5740195e5a3a0c0b6
Cyril Waechter <cyrwae@hotmail.com>
2025-02-25 20:49:17 +0100

    ParameterDelete: Added to UI and improved

--- pyRevitMEP.tab/Modify.panel/Other.stack/Delete.pulldown/ParameterDelete.pushbutton/script.py
+++ pyRevitMEP.tab/Modify.panel/Other.stack/Delete.pulldown/ParameterDelete.pushbutton/script.py
@@ -1,41 +1,19 @@
-"""
-Copyright (c) 2017 Cyril Waechter
-Python scripts for Autodesk Revit
+from Autodesk.Revit.DB import FilteredElementCollector, ParameterElement
 
-This file is part of pypevitmep repository at https://github.com/CyrilWaechter/pypevitmep
+from pyrevit import revit, forms
 
-pypevitmep is an extension for pyRevit. It contain free set of scripts for Autodesk Revit:
-you can redistribute it and/or modify it under the terms of the GNU General Public License
-version 3, as published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-See this link for a copy of the GNU General Public License protecting this package.
-https://github.com/CyrilWaechter/pypevitmep/blob/master/LICENSE
-"""
-
-from revitutils import doc
-
-# noinspection PyUnresolvedReferences
-from Autodesk.Revit.DB import Transaction, FilteredElementCollector, ParameterElement
-
-#Retrieve all parameters in the document
+doc = revit.doc
 params = FilteredElementCollector(doc).OfClass(ParameterElement)
-filteredparams = []
 
-#Store parameters which has a name starting with "magi" or "MC"
-for param in params:
-    if param.Name.startswith(("magi", "MC")): #startswith method accept tuple
-        filteredparams.append(param)
-        print param.Name
+selected_params = forms.SelectFromList.show(
+    params,
+    "Select parameters to delete",
+    multiselect=True,
+    name_attr="Name",
+)
 
-#Delete all parameters in the list
-t = Transaction(doc, "Delete parameters")
-t.Start()
-for param in filteredparams:
-    doc.Delete(param.Id)
-t.Commit()
 
+if selected_params:
+    with revit.Transaction("Delete ParameterElement"):
+        for param in selected_params:
+            doc.Delete(param.Id)
